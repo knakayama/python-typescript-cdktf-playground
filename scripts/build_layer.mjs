c6474202b22cc32a475e5abef1dae32af82e8978
knakayama <knakayama.sh@gmail.com>
2022-12-28 13:00:20 +0800

    fix: GitHub runner

--- scripts/build_layer.mjs
+++ scripts/build_layer.mjs
@@ -1,5 +1,5 @@
 #!/usr/bin/env zx
 
-cd(path.join(__dirname, '../../app'))
+cd(path.join(__dirname, '../app'))
 await $`poetry export --with main --without-hashes --format requirements.txt > requirements.txt`
 await $`pip install -r requirements.txt --target ./vendor/python --upgrade`
