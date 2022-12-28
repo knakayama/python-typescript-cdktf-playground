0a2a8d663893234846b8c4e22d3099036425d881
knakayama <knakayama.sh@gmail.com>
2022-12-28 12:53:38 +0800

    chore: update

--- infrastructure/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { App } from 'cdktf'
-import { API } from './components/stacks/api'
-
-const app = new App()
-
-new API(app, 'api')
-
-app.synth()
--- infrastructure/src/modules/types/index.ts
+++
@@ -1,4 +0,0 @@
-import { S3BackendProps } from 'cdktf'
-import { Except } from 'type-fest'
-
-export type BackendStorageProps = Except<S3BackendProps, 'key'>
